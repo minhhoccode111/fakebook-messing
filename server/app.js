9a9d05a6b820765094dd86d0569894bff89ca45b
minhhoccode111 <minhhoccode111@gmail.com>
2024-04-10 17:30:50 +0700

    add: working on cleanup template

--- server/app.js
+++ server/app.js
@@ -1,25 +1,27 @@
-const RateLimit = require('express-rate-limit');
-const createError = require('http-errors');
-const compression = require('compression');
-const express = require('express');
-const logger = require('morgan');
-const helmet = require('helmet');
-const path = require('path');
-require('dotenv').config();
+const RateLimit = require("express-rate-limit");
+const createError = require("http-errors");
+const compression = require("compression");
+const express = require("express");
+const logger = require("morgan");
+const helmet = require("helmet");
+const path = require("path");
+require("dotenv").config();
 
 // debug
-const debug = require('debug')('xxxxxxxxxxxxxxxxxxxx-debug-xxxxxxxxxxxxxxxxxxxx');
+const debug = require("debug")(
+  "xxxxxxxxxxxxxxxxxxxx-debug-xxxxxxxxxxxxxxxxxxxx",
+);
 
 // connect mongo db
-require('./mongoConfig');
+require("./mongoConfig");
 
 // db models, for authentication
-const User = require('./src/models/user');
+const User = require("./src/models/user");
 
 const app = express();
 
 // reduce fingerprinting
-app.disable('x-powered-by');
+app.disable("x-powered-by");
 
 // rate limit // TODO change to 120 in production
 const limiter = RateLimit({ windowMs: 1 * 60 * 1000, max: 1600 }); // max 160/min
@@ -32,32 +34,32 @@ app.use(compression());
 app.use(helmet());
 
 // setup CORS (Cross-origin Resources Sharing) to allow request from any origin
-const cors = require('cors');
+const cors = require("cors");
 // app.use(cors()); // TODO is used for development
 app.use(
   cors({
     origin: [
-      'http://localhost:5173', // development frontend
-      'http://localhost:4173', // development frontend preview
-      'http://localhost:3000', // development postman
-      'https://messagingapptop.vercel.app', // production
+      "http://localhost:5173", // development frontend
+      "http://localhost:4173", // development frontend preview
+      "http://localhost:3000", // development postman
+      "https://messagingapptop.vercel.app", // production
     ],
-    methods: 'GET,POST,PUT,DELETE', // simple CRUD actions
-  })
+    methods: "GET,POST,PUT,DELETE", // simple CRUD actions
+  }),
 );
 
 // basic setup
-app.use(logger('dev')); // logger
+app.use(logger("dev")); // logger
 app.use(express.json()); // parse json to js object
 app.use(express.urlencoded({ extended: false })); //  parse form data
-app.use(express.static(path.join(__dirname, 'public'))); // server things in public
+app.use(express.static(path.join(__dirname, "public"))); // server things in public
 
 // passport to authenticate a jwt
-const passport = require('passport');
+const passport = require("passport");
 // a passport strategy to authentication by passport.use(new JwtStrategy(options, verify))
-const JwtStrategy = require('passport-jwt').Strategy;
+const JwtStrategy = require("passport-jwt").Strategy;
 // to choose ways to extract json web token from request
-const ExtractJwt = require('passport-jwt').ExtractJwt;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
 // option jwt
 const options = {
   // extract json web token using Bearer in header
@@ -74,7 +76,10 @@ passport.use(
   new JwtStrategy(options, async (payload, done) => {
     try {
       // debug(`the payload object in passport.use: `, payload);
-      const user = await User.findOne({ username: payload.username }, '-password -username -__v').exec();
+      const user = await User.findOne(
+        { username: payload.username },
+        "-password -username -__v",
+      ).exec();
 
       // debug(`the user object in passport.use: `, user);
       if (!user) return done(null, false);
@@ -84,17 +89,25 @@ passport.use(
     } catch (err) {
       return done(err, false);
     }
-  })
+  }),
 );
 
 // handle api request
-const routes = require('./src/routes'); // modular
+const routes = require("./src/routes"); // modular
 // things about auth
-app.use('/api/v1/auth', routes.auth);
+app.use("/api/v1/auth", routes.auth);
 // things about user, need authenticate
-app.use('/api/v1/user', passport.authenticate('jwt', { session: false }), routes.user);
+app.use(
+  "/api/v1/user",
+  passport.authenticate("jwt", { session: false }),
+  routes.user,
+);
 // things about chat, need authenticate
-app.use('/api/v1/chat', passport.authenticate('jwt', { session: false }), routes.chat);
+app.use(
+  "/api/v1/chat",
+  passport.authenticate("jwt", { session: false }),
+  routes.chat,
+);
 
 // if no route handle the request mean it a 404
 app.use(function (req, res, next) {
@@ -106,7 +119,7 @@ app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   // we can only access the err object in res.locals.error if development, else it's an empty object
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // log the error
   debug(`the error object: `, err);
