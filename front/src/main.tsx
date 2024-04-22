3975d14ee084e723a87cd1051f99ab2e16ef497d
minhhoccode111 <minhhoccode111@gmail.com>
2024-04-22 18:38:42 +0700

    Add: Init project frontend

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Router from "./routes/router.tsx";
+import App from "./app.tsx";
 import "./css/index.css";
 import "./css/app.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Router />
+    <App />
   </React.StrictMode>,
 );
--- front/src/routes/layout.tsx
+++
@@ -1,3 +0,0 @@
-export default function Layout() {
-  return <div className="">Hello from layout</div>;
-}
